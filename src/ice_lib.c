c60afb939ece41505403e544a025a1f982017c1b
Giuseppe Lettieri <g.lettieri@iet.unipi.it>
2022-02-21 08:57:34 +0100

    netmap patch

--- src/ice_lib.c
+++ src/ice_lib.c
@@ -10,6 +10,12 @@
 #include "ice_vsi_vlan_ops.h"
 #include "ice_irq.h"
 
+#if defined(CONFIG_NETMAP) || defined(CONFIG_NETMAP_MODULE)
+#define NETMAP_ICE_LIB
+#include <ice_netmap_linux.h>
+#endif
+
+
 /**
  * ice_vsi_type_str - maps VSI type enum to string equivalents
  * @vsi_type: VSI type enum
@@ -3607,6 +3613,10 @@ ice_vsi_setup(struct ice_pf *pf, struct ice_vsi_cfg_params *params)
 	if (!vsi->agg_node)
 		ice_set_agg_vsi(vsi);
 
+#ifdef DEV_NETMAP
+    ice_netmap_attach(vsi);
+#endif
+
 	return vsi;
 
 err_vsi_cfg:
@@ -3923,6 +3933,9 @@ int ice_vsi_release(struct ice_vsi *vsi)
 	 */
 	if (vsi->netdev && !ice_is_reset_in_progress(pf->state) &&
 	    (test_bit(ICE_VSI_NETDEV_REGISTERED, vsi->state))) {
+#ifdef DEV_NETMAP
+        netmap_detach(vsi->netdev);
+#endif
 		unregister_netdev(vsi->netdev);
 		clear_bit(ICE_VSI_NETDEV_REGISTERED, vsi->state);
 	}
