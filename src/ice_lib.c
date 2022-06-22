be1d0f5fe47319d7d94fa304a98f58b9cf044529
Giuseppe Lettieri <g.lettieri@iet.unipi.it>
2022-06-22 21:06:39 +0200

    remote spurious hooks

--- src/ice_lib.c
+++ src/ice_lib.c
@@ -2946,10 +2946,6 @@ ice_vsi_setup(struct ice_pf *pf, struct ice_port_info *pi,
 	if (!vsi->agg_node)
 		ice_set_agg_vsi(vsi);
 
-#ifdef DEV_NETMAP
-    ice_netmap_attach(vsi);
-#endif
-
 	return vsi;
 
 unroll_clear_rings:
@@ -3298,9 +3294,6 @@ int ice_vsi_release(struct ice_vsi *vsi)
 	 */
 	if (vsi->netdev && !ice_is_reset_in_progress(pf->state) &&
 	    (test_bit(ICE_VSI_NETDEV_REGISTERED, vsi->state))) {
-#ifdef DEV_NETMAP
-        netmap_detach(vsi->netdev);
-#endif
 		unregister_netdev(vsi->netdev);
 		clear_bit(ICE_VSI_NETDEV_REGISTERED, vsi->state);
 
